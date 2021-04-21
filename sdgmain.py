fb8396777db6e4879316104373fd6c8df3ec9898
oterenari <81268817+oterenari@users.noreply.github.com>
2021-04-21 03:01:53 -0700

    Update sdgmain.py

--- sdgmain.py
+++ sdgmain.py
@@ -22,7 +22,7 @@ from sklearn.ensemble import RandomForestClassifier
     #st.dataframe(df)
     #st.table(df)
 
-pytrend = TrendReq()
+pytrends = TrendReq(hl='en-US', tz=360, timeout=(10,25), proxies=['https://34.203.233.13:80',], retries=2, backoff_factor=0.1, requests_args={'verify':False})
 
 def load_lottieurl(url: str):
     r = requests.get(url)
